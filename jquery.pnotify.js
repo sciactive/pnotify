b2e2cbd2b772ba16cb2066fb41b5f03f02caa0f7
Josh Graber <jgraber@renovo1.com>
2014-01-22 12:02:29 -0500

    Revert "Clicking the "Last" history button should open the most recent notice, not only the first notice ever opened. In addition, when using the "push": "top" stack option, the most recent notice is at the opposite end of the array so this should be accounted for."
    This reverts commit 32c35e678c3a590940b8918aaf445808abb349d1.

--- jquery.pnotify.js
+++ jquery.pnotify.js
@@ -773,28 +773,20 @@
 								$(this).removeClass(styles.hi_btnhov);
 							},
 							"click": function(){
-								// Get notices
-								var notices = jwindow.data("pnotify");
-								if (notices == null) {
-									return false;
-								}
-
-								var pushTop = ($.pnotify.defaults.stack.push == "top");
-
 								// Look up the last history notice, and display it.
-								var i = (pushTop ? 0 : -1);
-
+								var i = -1;
 								var notice;
 								do {
 									if (i == -1)
-										notice = notices.slice(i);
+										notice = notices_data.slice(i);
 									else
-										notice = notices.slice(i, i + 1);
+										notice = notices_data.slice(i, i+1);
 									if (!notice[0])
-										return false;
-
-									i = (pushTop ? i + 1 : i - 1);
+										break;
+									i--;
 								} while (!notice[0].pnotify_history || notice[0].is(":visible"));
+								if (!notice[0])
+									return false;
 								if (notice[0].pnotify_display)
 									notice[0].pnotify_display();
 								return false;
