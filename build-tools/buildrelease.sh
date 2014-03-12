6d3ad035879fb794a58ed1327dc232547503c881
Hunter Perrin <hperrin@gmail.com>
2014-03-12 10:13:46 -0700

    Fixed build release script to be able to be run from any working directory. Also made it zip up a folder instead of just the files. Closes #97.

--- build-tools/buildrelease.sh
+++ build-tools/buildrelease.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 
+DIR=$(dirname $0)
+
 echo Setting up directory structure.
-mkdir "jquery.pnotify"
-mkdir "jquery.pnotify/use for pines style icons"
+mkdir "${DIR}/pnotify"
+mkdir "${DIR}/pnotify/use for picon"
 echo Importing files.
-cp "../jquery.pnotify.default.icons.css" "jquery.pnotify/use for picon/"
-cp "../jquery.pnotify.default.css" "jquery.pnotify/"
-cp "../jquery.pnotify.js" "jquery.pnotify/"
+cp "${DIR}/../jquery.pnotify.default.icons.css" "${DIR}/pnotify/use for picon/"
+cp "${DIR}/../jquery.pnotify.default.css" "${DIR}/pnotify/"
+cp "${DIR}/../jquery.pnotify.js" "${DIR}/pnotify/"
 
 echo Compressing JavaScript with Google Closure Compiler.
-head -n 11 jquery.pnotify/jquery.pnotify.js > jquery.pnotify/jquery.pnotify.min.js
-java -jar compiler.jar --js=jquery.pnotify/jquery.pnotify.js >> jquery.pnotify/jquery.pnotify.min.js
+head -n 11 "${DIR}/pnotify/jquery.pnotify.js" > "${DIR}/pnotify/jquery.pnotify.min.js"
+java -jar "${DIR}/compiler.jar" --js="${DIR}/pnotify/jquery.pnotify.js" >> "${DIR}/pnotify/jquery.pnotify.min.js"
+
+CURDIR=$(pwd)
+cd "${DIR}/"
 
 echo Zipping the whole directory.
-cd jquery.pnotify/
-zip -r pnotify.zip .
-cd ..
-mv jquery.pnotify/pnotify.zip .
+zip -r pnotify.zip pnotify
 
 echo Cleaning up.
-rm -r jquery.pnotify/
+rm -r pnotify/
+
+cd "${CURDIR}/"
 
 echo Done.
