eccfca22ad1929f47096fd2e25cd1e20ec5044e3
Hunter Perrin <hperrin@gmail.com>
2014-04-09 13:34:31 -0700

    A script to build custom source files.

---
+++ buildcustom.php
@@ -0,0 +1,42 @@
+<?php
+
+$type = (false ? 'inline' : 'attachment');
+$ext = ($_REQUEST['mode'] !== "css" ? "js" : "css");
+$mime = ($_REQUEST['mode'] !== "css" ? "text/javascript" : "text/css");
+$files = explode('-', preg_replace("/[^a-z0-9-]/", '', substr($_REQUEST['files'], 0, 256)));
+if (!$files) {
+	header('HTTP/1.1 400 Bad Request', true, 400);
+	echo "Your request could not be completed because you did not specify any files.";
+	exit;
+}
+sort($files);
+$min = !empty($_REQUEST['min']) ? 'min.' : '';
+$cache_file = 'build-cache/pnotify-'.implode('-', $files).'.'.$min.$ext;
+
+if (file_exists($cache_file)) {
+	$content = file_get_contents($cache_file);
+	header("Content-Disposition: $type; filename=pnotify.custom.$min$ext");
+	header("Content-Length: ".strlen($content));
+	header("Content-Type: $mime");
+	echo $content;
+	exit;
+}
+
+$content = file_get_contents("pnotify.core.$min$ext");
+foreach ($files as $cur_file) {
+	$filename = "pnotify.$cur_file.$min$ext";
+	if (!file_exists($filename)) {
+		header('HTTP/1.1 400 Bad Request', true, 400);
+		echo "Your request could not be completed because a file you requested is invalid.";
+		exit;
+	}
+	$content .= file_get_contents($filename);
+}
+
+
+header("Content-Disposition: $type; filename=pnotify.custom.$min$ext");
+header("Content-Length: ".strlen($content));
+header("Content-Type: $mime");
+echo $content;
+
+file_put_contents($cache_file, $content);
