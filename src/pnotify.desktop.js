f8c74c67859f544828f462e276e5f73bb93eacc5
Hunter Perrin <hperrin@gmail.com>
2017-04-21 14:27:42 -0700

    Minified new desktop module code.

--- src/pnotify.desktop.js
+++ src/pnotify.desktop.js
@@ -52,7 +52,11 @@
         // Using a tag lets you update an existing notice, or keep from duplicating notices between tabs.
         // If you leave tag null, one will be generated, facilitating the "update" function.
         // see: http://www.w3.org/TR/notifications/#tags-example
-        tag: null
+        tag: null,
+        // Optionally display a different title for the desktop.
+        title: null,
+        // Optionally display different text for the desktop.
+        text: null
     };
     PNotify.prototype.modules.desktop = {
         tag: null,
