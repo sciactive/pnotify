5561860c31ba384836d37a66b918e6f0b5d0257f
Hunter Perrin <hperrin@gmail.com>
2020-04-27 15:00:44 -0700

    build: switch core to rollup build

---
+++ packages/core/rollup.config.js
@@ -0,0 +1,52 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import svelte from 'rollup-plugin-svelte';
+import babel from 'rollup-plugin-babel';
+import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
+import path from 'path';
+import pkg from './package.json';
+const devMode = process.env.ENV === 'development';
+
+const cssFiles = ['BrightTheme', 'Material'];
+
+export default [
+  {
+    input: pkg.svelte,
+    output: {
+      file: pkg.main,
+      format: 'umd',
+      name: path.basename(pkg.main, '.js'),
+      exports: 'named'
+    },
+    plugins: [
+      nodeResolve({
+        browser: true
+      }),
+      svelte({
+        emitCss: true
+      }),
+      babel({
+        presets: ['@babel/preset-env'],
+        extensions: ['.js', '.mjs', '.svelte']
+      }),
+      postcss({
+        extract: true,
+        minimize: !devMode
+      }),
+      ...(devMode ? [] : [terser()])
+    ]
+  },
+  ...cssFiles.map(name => ({
+    input: `${name}.css`,
+    output: {
+      file: `dist/${name}.js`,
+      format: 'cjs'
+    },
+    plugins: [
+      postcss({
+        extract: true,
+        minimize: !devMode
+      })
+    ]
+  }))
+];
