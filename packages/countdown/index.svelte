ec5d7f13b31f81fda7cea57ef1baa67f7c99f8f2
Hunter Perrin <hperrin@gmail.com>
2020-11-06 11:24:26 -0800

    fix: fix countdown bar appearance in Angeler theme

--- packages/countdown/index.svelte
+++ packages/countdown/index.svelte
@@ -29,6 +29,23 @@
     self.delay !== Infinity;
   $: timeStart =
     showCountdown && _timer && _timer !== 'prevented' ? new Date() : null;
+  $: {
+    self.removeModuleClass(
+      'elem',
+      'pnotify-with-countdown',
+      'pnotify-with-countdown-bottom',
+      'pnotify-with-countdown-top',
+      'pnotify-with-countdown-left',
+      'pnotify-with-countdown-right'
+    );
+    if (showCountdown) {
+      self.addModuleClass(
+        'elem',
+        'pnotify-with-countdown',
+        `pnotify-with-countdown-${anchor}`
+      );
+    }
+  }
 
   const getValues = () => {
     _state = self.getState();
